0ba18ec58802f97a496995617d4bcc7cbfe8d8e2
ekazantsev <ekazantsev@ocs.ru>
2025-03-20 23:22:32 +0400

    добавлены зависимости.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "ru.kazantsev.nsd.sdk"
-version = "1.0.2"
+version = "1.1.1"
 
 java {
     withSourcesJar()
@@ -37,16 +37,16 @@ repositories {
 }
 
 dependencies {
-    implementation("com.google.code.findbugs:jsr305:3.0.2")
-    implementation("javax.activation:javax.activation-api:1.2.0")
-    implementation("org.springframework:spring-web:6.1.5")
-    implementation("org.apache.poi:poi-ooxml:5.2.5")
-    implementation("org.apache.poi:poi:5.2.5")
-    implementation("org.slf4j:slf4j-api:2.0.16")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.google.guava:guava:33.1.0-jre")
-    implementation("org.gwtproject:gwt-user:2.10.0")
+    implementation ("org.slf4j:slf4j-api:2.0.16")
+    api ("com.google.code.findbugs:jsr305:3.0.2")
+    api ("javax.activation:javax.activation-api:1.2.0")
+    api ("org.springframework:spring-web:6.1.5")
+    api ("org.apache.poi:poi-ooxml:5.2.5")
+    api ("org.apache.poi:poi:5.2.5")
+    api ("com.google.code.gson:gson:2.10.1")
+    api ("com.google.guava:guava:33.1.0-jre")
+    api ("org.gwtproject:gwt-user:2.10.0")
     //Мне говорили что его когда нибудь выпилят из СД
-    implementation("org.codehaus.groovy.modules.http-builder:http-builder:0.7.1")
+    api ("org.codehaus.groovy.modules.http-builder:http-builder:0.7.1")
 }
 
