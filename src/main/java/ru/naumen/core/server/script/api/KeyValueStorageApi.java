2053fed40ba52c40225a23d56f7a92ed634f29fe
ekazantsev <ekazantsev@ocs.ru>
2025-04-02 18:08:49 +0400

    добавлены зависимости.

---
+++ src/main/java/ru/naumen/core/server/script/api/KeyValueStorageApi.java
@@ -0,0 +1,8 @@
+package ru.naumen.core.server.script.api;
+
+public class KeyValueStorageApi {
+    public java.lang.String get(java.lang.String nameSpace, java.lang.String key) {return null;}
+    public boolean put(java.lang.String nameSpace, java.lang.String key, java.lang.String value) {return false;}
+    public java.util.Map find(java.lang.String nameSpace, java.lang.String keyPart, ru.naumen.core.shared.filters.KeyValuePredicate predicate) {return null;}
+    public boolean delete(java.lang.String nameSpace, java.lang.String key) {return false;}
+}
---
+++ src/main/java/ru/naumen/core/shared/filters/KeyValuePredicate.java
@@ -0,0 +1,5 @@
+package ru.naumen.core.shared.filters;
+
+public abstract class KeyValuePredicate {
+    public abstract boolean check(java.lang.String key, java.lang.String value);
+}
